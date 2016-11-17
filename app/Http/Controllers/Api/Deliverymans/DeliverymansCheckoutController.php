fef04a413d6f2a91a7278f7b6532c1f5cd4442a4
Tassio Pinheiro <tascarpin@yahoo.com.br>
2016-11-17 20:13:16 -0200

    Terminado deliverymansCheckoutController.

---
+++ app/Http/Controllers/Api/Deliverymans/DeliverymansCheckoutController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace CodeDelivery\Http\Controllers\Api\Deliverymans;
+
+use CodeDelivery\Http\Controllers\Controller;
+use CodeDelivery\Repositories\OrderRepository;
+use CodeDelivery\Repositories\UserRepository;
+use CodeDelivery\Services\OrderService;
+use Illuminate\Http\Request;
+use LucaDegasperi\OAuth2Server\Facades\Authorizer;
+
+class DeliverymansCheckoutController extends Controller
+{
+    use OrderService;
+
+    /**
+     * @var OrderRepository
+     */
+    private $orderRepository;
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(
+        OrderRepository $orderRepository,
+        UserRepository $userRepository
+    )
+    {
+        $this->orderRepository = $orderRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    public function index()
+    {
+        $id = Authorizer::getResourceOwnerId();
+        $orders = $this->orderRepository->with('items')->scopeQuery(function($query) use($id){
+            return $query->where('user_deliveryman_id', '=', $id);
+        })->paginate();
+
+        return $orders;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $idDeliveryman = Authorizer::getResourceOwnerId();
+        $object = $this->getByIdAndDeliverymanService($id, $idDeliveryman);
+        return $object;
+    }
+
+    public function updateStatus(Request $request, $id)
+    {
+        $idDeliveryman = Authorizer::getResourceOwnerId();
+        $order = $this->updateStatusService($id, $idDeliveryman, $request->get('status'));
+        if($order){
+            return $order;
+        }
+        abort(400, 'Order não encontrado');
+    }
+}
